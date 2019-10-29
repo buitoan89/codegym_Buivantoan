9f0cff4886c7924c3954f39ff404ac4667fd6e95
Your Name <buivantoan08101989@gmail.com>
2019-10-29 10:06:41 +0700

    bai tap

---
+++ Sesion_03_câu lệnh điều kiện lặp nhảy/game puzzle/js.js
@@ -0,0 +1,18 @@
+
+let image1 = 'a';
+let image2 = 'b';
+let image3 = 'c';
+let image4 = 'a';
+let image5 = 'c';
+ let khung = document.getElementById("khung");
+ khung.style.backgroundColor = 'while';
+ khung.style.boxShadow = 'while';
+let hang1= document.getElementById('hang1');
+    hang1.style.backgroundImage = 'url(anh/funny-cat1_part1x1.jpg)';
+ // function chuyenanh1() {
+ //     let hang1= document.getElementById('hang1');
+ //     hang1.style.backgroundImage = 'url(anh/funny-cat1_part1x1.jpg)';
+ //     if (image === 'a'){
+ //         hang1.style.backgroundImage = 'url(/anh/funny-cat1_part1x1.jpg)';
+ //     }
+ // }
